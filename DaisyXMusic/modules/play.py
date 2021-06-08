2c3e7569802be0c02e9cd622e274e4f33a563e4e
Anshul Rajput <82425167+LUCKYRAJPUTOP@users.noreply.github.com>
2021-06-08 10:11:13 +0530

    Update play.py

--- DaisyXMusic/modules/play.py
+++ DaisyXMusic/modules/play.py
@@ -1,4 +1,4 @@
-# Daisyxmusic (Telegram bot project)
+# MahakalMusic (Telegram bot project)
 # Copyright (C) 2021  Inukaasith
 # Copyright (C) 2021  TheHamkerCat (Python_ARQ)
 # This program is free software: you can redistribute it and/or modify
@@ -33,23 +33,23 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 from Python_ARQ import ARQ
 from youtube_search import YoutubeSearch
 
-from DaisyXMusic.config import ARQ_API_KEY
-from DaisyXMusic.config import BOT_NAME as bn
-from DaisyXMusic.config import DURATION_LIMIT
-from DaisyXMusic.config import UPDATES_CHANNEL as updateschannel
-from DaisyXMusic.config import que
-from DaisyXMusic.function.admins import admins as a
-from DaisyXMusic.helpers.admins import get_administrators
-from DaisyXMusic.helpers.channelmusic import get_chat_id
-from DaisyXMusic.helpers.errors import DurationLimitError
-from DaisyXMusic.helpers.decorators import errors
-from DaisyXMusic.helpers.decorators import authorized_users_only
-from DaisyXMusic.helpers.filters import command, other_filters
-from DaisyXMusic.helpers.gets import get_file_name
-from DaisyXMusic.services.callsmusic import callsmusic, queues
-from DaisyXMusic.services.callsmusic.callsmusic import client as USER
-from DaisyXMusic.services.converter.converter import convert
-from DaisyXMusic.services.downloaders import youtube
+from MahakalMusic.config import ARQ_API_KEY
+from MahakalMusic.config import BOT_NAME as bn
+from MahakalMusic.config import DURATION_LIMIT
+from MahakalMusic.config import UPDATES_CHANNEL as updateschannel
+from MahakalMusic.config import que
+from MahakalMusic.function.admins import admins as a
+from MahakalMusic.helpers.admins import get_administrators
+from MahakalMusic.helpers.channelmusic import get_chat_id
+from MahakalMusic.helpers.errors import DurationLimitError
+from MahakalMusic.helpers.decorators import errors
+from MahakalMusic.helpers.decorators import authorized_users_only
+from MahakalMusic.helpers.filters import command, other_filters
+from MahakalMusic.helpers.gets import get_file_name
+from MahakalMusic.services.callsmusic import callsmusic, queues
+from MahakalMusic.services.callsmusic.callsmusic import client as USER
+from MahakalMusic.services.converter.converter import convert
+from MahakalMusic.services.downloaders import youtube
 
 aiohttpsession = aiohttp.ClientSession()
 chat_id = None
@@ -648,7 +648,7 @@ async def deezer(client: Client, message_: Message):
     try:
         user = await USER.get_me()
     except:
-        user.first_name = "DaisyMusic"
+        user.first_name = "MahakalMusic"
     usar = user
     wew = usar.id
     try:
@@ -776,7 +776,7 @@ async def jiosaavn(client: Client, message_: Message):
     try:
         user = await USER.get_me()
     except:
-        user.first_name = "DaisyMusic"
+        user.first_name = "MahakalMusic"
     usar = user
     wew = usar.id
     try:
@@ -813,7 +813,7 @@ async def jiosaavn(client: Client, message_: Message):
                     # print(e)
                     await lel.edit(
                         f"<b>🔴 Flood Wait Error 🔴 \nUser {user.first_name} couldn't join your group due to heavy requests for userbot! Make sure user is not banned in group."
-                        "\n\nOr manually add @DaisyXmusic to your Group and try again</b>",
+                        "\n\nOr manually add @MahakalPlayer to your Group and try again</b>",
                     )
     try:
         await USER.get_chat(chid)
